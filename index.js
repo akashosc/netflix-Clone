fd5ddafafeab437b15ad16416d0d2997a157cd4b
akashosc <84351117+akashosc@users.noreply.github.com>
2023-08-06 00:57:38 +0530

    add ../

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const userRoutes = require("../routes/UserRoutes");
+const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
 
 
@@ -14,7 +14,7 @@ const MONGO_URL=process.env.MONGO_CONNECT;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'../netflix-ui/build')));
+app.use(express.static(path.join(__dirname,'./netflix-ui/build')));
 
 mongoose
   .connect(MONGO_URL, {
@@ -29,7 +29,7 @@ mongoose
   });
 
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../netflix-ui/build/index.html'));
+    res.sendFile(path.join(__dirname,'./netflix-ui/build/index.html'));
   })
 
 app.use("/api/user", userRoutes);
